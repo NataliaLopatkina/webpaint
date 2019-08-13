9cbd0e07e2154b485d9f36ae01e570e97209f1b6
natalialopatkina <natalya.lopatkina1994@gmail.com>
2019-08-13 13:14:42 +0300

    Add project webpaint

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+"use strict";
+
+var gulp = require("gulp");
+var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var run= require("run-sequence");
+var del = require("del");
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: "./build",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("less/**/*.less", ["style-dev"]).on("change", server.reload);
+  gulp.watch("less/**/*.less").on("change", server.reload);
+  gulp.watch("*.html", ["copy-html"]).on("change", server.reload);
+});
+
+gulp.task("style", function() {
+  gulp.src("less/style.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream())
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("style-dev", function() {
+  gulp.src("less/style.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"));
+});
+
+
+gulp.task("images", function () {
+  return gulp.src("img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("images-dev", function () {
+  return gulp.src("img/**/*.{png,jpg,svg}")
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("build", function (done) {
+  run("clean","style", "images", "copy", done);
+});
+
+gulp.task("build-dev", function (done) {
+  run("style-dev", done);
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+    .pipe(gulp.dest("build"))
+});
+
+gulp.task("copy-html", function () {
+  return gulp.src([
+    "*.html"
+  ], {
+    base: "."
+  })
+    .pipe(gulp.dest("build"))
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
---
+++ js/script.js
@@ -0,0 +1,70 @@
+// Open/close menu
+
+document.addEventListener('DOMContentLoaded', function () {
+  var navList = document.querySelector(".nav");
+  var navButton = document.querySelector(".button-menu");
+  var navContent = document.querySelector(".nav__list");
+  var navOpened = document.querySelector(".nav");
+
+  navButton.addEventListener("click", function () {
+    var listHeight = navContent.offsetHeight;
+    if (navList.classList.contains("nav--closed")) {
+      navList.classList.remove("nav--closed");
+      navList.classList.add("nav--opened");
+      navOpened.style.height = listHeight + "px";
+      navButton.classList.remove("button-menu--open");
+      navButton.classList.add("button-menu--close");
+    } else {
+      navList.classList.add("nav--closed");
+      navList.classList.remove("nav--opened");
+      navOpened.style.height = "0px";
+      navButton.classList.remove("button-menu--close");
+      navButton.classList.add("button-menu--open");
+    }
+  });
+});
+
+// Filter
+
+var filterActive = ''
+
+function filterProducts(category) {
+    if (filterActive != category) {
+        var elements = document.querySelectorAll(".portfolio-filter__content-item");
+
+        elements.forEach(function (item, i) {
+          item.classList.add("not-active");
+        });
+
+        var activeElements = document.querySelectorAll(`[data-filter="${category}"]`);
+
+        activeElements.forEach(function (item) {
+          item.classList.remove("not-active");
+        });
+
+        filterActive = category;
+    }
+}
+
+var buttons = document.querySelectorAll(".button-filter");
+console.log(buttons);
+
+buttons.forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    var curretTarget = event.target.dataset.button
+    filterProducts(curretTarget);
+  })
+});
+
+  var buttonAll = document.querySelector(".button-all");
+  var elements = document.querySelectorAll(".portfolio-filter__content-item");
+
+  buttonAll.onclick = function () {
+    elements.forEach(function (item, i) {
+      item.classList.remove("not-active");
+    });
+
+    filterActive = "all";
+  };
+
+
